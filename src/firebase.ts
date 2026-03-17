b9955cc22e0a4b63461e1ac5f39cea872ae88474
MedNewton <med.newton.2013@gmail.com>
2026-03-17 20:47:14 +0000

    .

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,9 @@
 import admin from "firebase-admin";
 import { env } from "./env.js";
 
-const serviceAccount = JSON.parse(env.firebaseServiceAccount);
+const serviceAccount = JSON.parse(
+  Buffer.from(env.firebaseServiceAccount, "base64").toString("utf-8")
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
